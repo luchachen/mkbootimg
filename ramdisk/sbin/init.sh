ca18cda4412a00fae18823a0b0209ce01e410024
lucha.chen <lucha.chen@gmail.com>
2018-08-13 13:43:43 +0800

    add support armv7 and armv8

--- ramdisk/sbin/init.sh
+++ ramdisk/sbin/init.sh
@@ -1,13 +1,12 @@
 #!/system/bin/sh
-/system/bin/touch /dev/x
-/system/bin/cp -rd --preserve=all /system/etc/ /dev/
-/system/bin/sed -i -e 's/secure=1/secure=0/' -e 's/ro.debuggable=0/ro.debuggable=1/' /dev/etc/prop.default
-/system/bin/echo ''  > /dev/etc/selinux/plat_and_mapping_sepolicy.cil.sha256
-/system/bin/echo -n '/sbin/adbd    u:object_r:adbd_exec:s0
+PATH=/sbin:/system/sbin:/system/bin:/system/xbin:/vendor/bin:/vendor/xbin
+cp -rd --preserve=all /system/etc/ /dev/
+sed -i -e 's/secure=1/secure=0/' -e 's/ro.debuggable=0/ro.debuggable=1/' /dev/etc/prop.default
+echo ''  > /dev/etc/selinux/plat_and_mapping_sepolicy.cil.sha256
+echo -n '/sbin/adbd    u:object_r:adbd_exec:s0
 /sbin/init    u:object_r:init_exec:s0
 /sbin/init\.sh u:object_r:init_exec:s0
 ' >> /dev/etc/selinux/plat_file_contexts
-/system/bin/cat /sbin/su.cil >> /dev/etc/selinux/plat_sepolicy.cil
-/system/bin/cat /sbin/dontauditsu.cil >> /dev/etc/selinux/plat_sepolicy.cil
+cat /sbin/su.cil /sbin/dontauditsu.cil >> /dev/etc/selinux/plat_sepolicy.cil
 
-/system/bin/mount -o bind  /dev/etc/ /system/etc/
+mount -o bind  /dev/etc/ /system/etc/
