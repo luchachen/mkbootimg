da2de0c4f02b9462c9228e690c8790769aa10420
lucha.chen <lucha.chen@gmail.com>
2018-08-20 17:35:46 +0800

    add resize logcat ring buffer add logcat2uart

--- ramdisk/sbin/init.sh
+++ ramdisk/sbin/init.sh
@@ -2,6 +2,8 @@
 PATH=/sbin:/system/sbin:/system/bin:/system/xbin:/vendor/bin:/vendor/xbin
 cp -rd --preserve=all /system/etc/ /dev/
 sed -i -e 's/secure=1/secure=0/' -e 's/ro.debuggable=0/ro.debuggable=1/' /dev/etc/prop.default
+echo 'ro.logd.size=1M' >> /dev/etc/prop.default
+echo 'ro.logd.kernel=true' >> /dev/etc/prop.default
 echo ''  > /dev/etc/selinux/plat_and_mapping_sepolicy.cil.sha256
 echo -n '/sbin/adbd    u:object_r:adbd_exec:s0
 /sbin/init    u:object_r:init_exec:s0
